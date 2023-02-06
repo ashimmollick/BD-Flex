2aba0e4a9dbae3e6e8e3bb6fcbd918618ac2b141
SOURAVMONDAL-BD <souravmondalcode@gmail.com>
2023-02-06 14:16:44 +0600

    category page create

---
+++ bd-flix/src/Components/Banner/admin/Layout/AllCategories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllCategories = () => {
+    return (
+        <div>
+            <p>All Categories</p>
+        </div>
+    );
+};
+
+export default AllCategories;

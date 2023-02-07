707bc7ed18ef33cdfa1cc691bfbe23b4346ab44b
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2023-02-07 19:06:10 +0600

    added footer and edit navbar

--- bd-flix/src/Components/Navbar/Navbar.jsx
+++ bd-flix/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
-import { FaHome, FaVideo, MdDarkMode, FaToggleOn, FaToggleOff } from 'react-icons/fa';
+import { FaHome, FaVideo, FaToggleOn, FaToggleOff } from 'react-icons/fa';
 import { FaEnvelope } from 'react-icons/fa';
 import { AuthContext } from '../Context/Authprovider/Authprovider';
 import logo from '../../images/brand.png'
@@ -89,17 +89,9 @@ const Navbar = () => {
     }
     return (
         <>
-            <div className={`navbar bg-black`}>
+            {/* <div className={`navbar bg-black`}>
 
                 <div className="navbar-start">
-                    {/* <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            {nav}
-                        </ul>
-                    </div> */}
 
                     <div className='flex gap-2'>
                         <div className='btn rounded font-mono uppercase bg-none shadow-inner text-xl font-bold text-white'><img src={logo} alt=''></img>-FLIX</div>
@@ -135,14 +127,6 @@ const Navbar = () => {
                         <>
                             <li><Link to="/admin" className="text-white font-bold mr-10 hover:text-green-400 focus:outline-none focus:shadow-outline">  Admin</Link></li>
 
-
-
-
-
-
-
-
-
                             <div className="dropdown dropdown-end">
                                 <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                                     <div className="w-10 rounded-full">
@@ -160,17 +144,90 @@ const Navbar = () => {
                         <li><Link to='/login' className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">  Login</Link></li>
 
                     }
-
-                    {/* <button onClick={handleLogOut} className='btn bg-orange-600 rounded-3xl text-white'>LogOut</button> */}
                 </div>
+            </div> */}
+
+            <div className="navbar bg-[#2c2642]">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            {/* <svg xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg> */}
+                            <div className='btn rounded font-mono uppercase bg-none shadow-inner text-sm lg:text-xl font-bold text-white'><img className='w-12' src={logo} alt=''></img>-FLIX</div>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {nav}
+                        </ul>
+                    </div>
+
+                    <div className='hidden lg:block'>
+                        <div className='btn rounded font-mono uppercase bg-none shadow-inner text-sm lg:text-xl font-bold text-white'><img className='w-12' src={logo} alt=''></img>-FLIX</div>
+                    </div>
+
+
+                    <div className="dropdown">
+                        <label tabIndex={0} ><input type='text' placeholder='Search' value={filterVal} onInput={(e) => handleFilter(e)} className="input block w-48 lg:w-full h-8 lg:h-10 rounded-3xl bg-[#3a3b3c]" /></label>
+                        <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>{
+                                AllMoviesSearch?.slice(0, 3).map(it => {
+                                    return (
+
+                                        <Link to={`/allmovie/${it.id}` && `/clickedvideo/${it.id}` && `/moviesforyou/${it.id}`} key={it.id}>
+                                            {it.original_title.toLowerCase()}
+
+                                        </Link>
+
+                                    );
+                                }
+                                )
+                            }</li>
+
+                        </ul>
+                    </div>
+
+
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {nav}
+                    </ul>
+                </div>
+
+                <div className="navbar-end">
+                    {user?.uid ?
+                        <>
+                            <li><Link to="/admin" className="text-white font-bold mr-10 hover:text-green-400 focus:outline-none focus:shadow-outline">  Admin</Link></li>
+
+                            <div className="dropdown dropdown-end">
+                                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img src={user?.photoURL} alt="UserImage" />
+                                    </div>
+                                </label>
+                                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                                    <li className="text-white font-bold hover:text-green-400 focus:outline-none focus:shadow-outline" ><Link to='/profile'>Profile</Link></li>
+                                    <li className="text-white font-bold hover:text-green-400 focus:outline-none focus:shadow-outline" onClick={handlelogout}><Link to='/login'>Logout</Link></li>
+                                </ul>
+                            </div>
+
+                        </>
+                        :
+                        <li><Link to='/login' className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">  Login</Link></li>
+
+                    }
+                </div>
+
             </div>
+
+
 
             {/* ****************************************************************** */}
             {/* bottom navigation bar */}
             {/* ****************************************************************** */}
 
             <div className="lg:hidden md:hidden fixed bottom-0 w-full z-50">
-                <div className="bg-black shadow-lg px-6">
+                <div className="bg-[#2c2642] shadow-lg px-6">
                     <div className="flex items-center justify-between">
                         {
                             bottomNav
